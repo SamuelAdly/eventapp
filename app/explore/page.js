cbf568ffa8a4bbbc8d0f8fb6d84420b2545c93d5
Armand Soto <armand.e.soto@gmail.com>
2024-12-10 14:34:30 -0500

    Created Explore page and temporary list of events for display

--- app/explore/page.js
+++ app/explore/page.js
@@ -1,42 +1,37 @@
 'use client';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import EventCard from "../components/EventCard";
-
-const events = [
-    {
-      id: 1,
-      title: "Yoga Class",
-      date: "2024-12-10",
-      location: { lat: 40.7128, lng: -74.006 },
-      description: "Join us for a relaxing yoga session in the park.",
-    },
-    {
-      id: 2,
-      title: "Art Fair",
-      date: "2024-12-15",
-      location: { lat: 40.7306, lng: -73.9352 },
-      description: "Explore local art and meet the artists.",
-    },
-  ];
 
 
 
 
 const EventList = () => {
-    // const {title, date, location, description} = localEvent;
     
+    const [events, setEvents] = useState([]);
+    
+    useEffect(() => {
+      const fetchEvents = async () => {
+        const response = await fetch("/events.json"); 
+        const data = await response.json();
+        setEvents(data);
+      };
+      fetchEvents();
+    }, []);
+
     const handleRSVP = (eventId) => {
         alert(`RSVPed to event ID : ${eventId}`);
     }
     
     return(
         <div className="min-h-screen bg-gradient-to-b from-blue-500 to-indigo-600 text-white">
+            { /* Page header */}
             <section className="container mx-auto px-6 py-16 text-center">
-
                 <h1 className="text-4xl font-bold mb-6">Community Events</h1>
             </section>
+            { /* Events */}
+
             <div className="container mx-auto p-6">
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <div className="grid grid-cols-3 gap-y-4">
                 {events.map((event) => (
                     <EventCard key={event.id} localEvent={event} onRSVP={handleRSVP} />
                 ))}
