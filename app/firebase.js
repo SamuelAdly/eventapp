5bfa009f16ceb46993a57358d2ef2afa91a909dc
Aiden Schulman <aidenhschulman@gmail.com>
2024-12-10 17:59:46 -0500

    Removed emulator for development

--- app/firebase.js
+++ app/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { connectAuthEmulator, getAuth } from "firebase/auth";
-import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -19,9 +19,3 @@ const app = initializeApp(firebaseConfig);
 // Instantiate Services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-// Emulator
-if (process.env.NODE_ENV == "development") {
-    connectAuthEmulator(auth, "http://127.0.0.1:9099");
-    connectFirestoreEmulator(db, "127.0.0.1", 8080);
-}
